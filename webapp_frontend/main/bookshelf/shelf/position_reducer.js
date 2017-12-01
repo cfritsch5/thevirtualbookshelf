977753c6f0ae61e5b209972f85d4d7ae327f3b3c
casey fritsch <caseyfritsch@gmail.com>
2017-12-01 14:58:22 -0800

    calculate points

--- webapp_frontend/main/bookshelf/shelf/position_reducer.js
+++ webapp_frontend/main/bookshelf/shelf/position_reducer.js
@@ -1,12 +1,60 @@
 import {merge} from 'lodash';
 
+let position = {
+  x: 0,
+  y: 0,
+  angle: 0,
+  width: 0,
+  depth: 0,
+  A: { x: null, z: null },
+  B: { x: null, z: null },
+  C: { x: null, z: null },
+  D: { x: null, z: null },
+};
+
 const PositionReducer = (state = {}, action) => {
   // console.log("POSITION REDUCER");
   switch (action.type) {
     case "NEW_POSITION":
       // console.log("position action", action,state);
+      // let positions = {};
+      let _id = action.position.id;
+      let positions = {};
       // let newPos = {[action.position.id]: {angle: action.pos.angle}};
-      return merge({}, state, action.position);
+      Object.keys(action.position).forEach((id)=>{
+        let newPos = merge({},position, state[id], action.position[id]);
+        let x = newPos.x;
+        let angle = newPos.angle;
+        let {width, depth} = newPos;
+        // let x = action.position[id].x || state.x || 0;
+        // let angle = action.position[id].angle || state.angle || 0;
+        // let {width, depth} = state[id] || action.position[id];
+        // points = {
+        //   A: { x: x,     z: 0 },
+        //   B: { x: width*Math.cos(angle*Math.PI/180), z: width*Math.sin(angle*Math.PI/180) },
+        //   C: { x: depth*Math.cos(angle*Math.PI/180), z: depth*Math.sin(angle*Math.PI/180) },
+        //   D: { x: width, z: depth },
+        // };
+        // console.log( position);
+        newPos.A.x = x;
+        newPos.A.z = 0;
+        newPos.B.x = Math.round(width*Math.cos(angle*Math.PI/180));
+        newPos.B.z = Math.round((width/2)*Math.sin(angle*Math.PI/180));
+        newPos.C.x = Math.round(depth*Math.sin(angle*Math.PI/180));
+        newPos.C.z = Math.round(depth*Math.cos(angle*Math.PI/180));
+        newPos.D.x = Math.round(newPos.C.x + newPos.B.x);
+        newPos.D.z = Math.round(newPos.C.z + newPos.B.z);
+
+        newPos.C.z = newPos.C.z  - newPos.B.z;
+        newPos.B.x = newPos.B.x + x;
+        newPos.C.x = newPos.C.x + x;
+        newPos.D.x = newPos.D.x + x;
+
+        // console.log(merge(position,points));
+        positions[id] = newPos;
+      });
+      // let new_pos = merge( action.position, points);
+      return merge({}, state, positions);
     default:
       return state;
   }
